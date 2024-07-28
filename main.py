6260ee3bcaa42dd8637cd5b50c237a01caf02eea
Omar Ahmed <omar.tolis2004@gmail.com>
2024-07-28 15:11:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,17 +11,30 @@ def P1():
     for word in text:
         if len(word) > 10:
             print(word[0] + str(len(word) - 2) + word[-1])
-        elif(len(word) <= num or (len(word)>num and len(word)<=10 )):
+        elif len(word) <= num or (len(word) > num and len(word) <= 10):
             print(word)
+
 
 #######################################################################
 
-# # B. A Team
-# def P2():
-#     num = int(input())
-#     numbers = [][]
-#
-#     # Read words into the list
-#     for _ in range(num):
-#         for _ in range(i=0):
-#             numbers[][]=(int(input()))
+# B. A Team
+num = int(input())
+numbers = []  # Initialize an empty list to hold the sublist
+
+# Read numbers into the list
+for _ in range(num):
+    row = []  # Create a new list for each row
+    for i in range(3):
+        row.append(int(input()))
+    numbers.append(row)  # Add the row to the numbers list
+
+count = 0
+
+for number in range(num):
+    for i in range(2):
+        if numbers[number][i] == numbers[number][i + 1] or numbers[number][i] == numbers[number][i + 2] or \
+                numbers[number][i] == numbers[number][i + 2] == numbers[number][i + 1]:
+            count += 1
+    return count
+
+# print(numbers)
