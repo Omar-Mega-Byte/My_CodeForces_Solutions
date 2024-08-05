97f1c318b5590537e778e3a6fb2a8781c5a064f5
Omar Ahmed <omar.tolis2004@gmail.com>
2024-08-05 09:49:15 +0300

    Update #50A - Domino piling.py

--- #50A - Domino piling.py
+++ #50A - Domino piling.py
@@ -1,6 +1,5 @@
-def P5():
-    data = list(map(int, input().split()))  # Read n and k
-    n = data[0]
-    k = data[1]
-    mul = n * k
-    print(mul // 2)
+data = list(map(int, input().split()))  # Read n and k
+n = data[0]
+k = data[1]
+mul = n * k
+print(mul // 2)
