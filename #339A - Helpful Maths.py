b3c23fb2a0468be8b32e4b8e18bab2e0d44586aa
Omar Ahmed <omar.tolis2004@gmail.com>
2024-08-05 09:48:42 +0300

    Update #339A - Helpful Maths.py

--- #339A - Helpful Maths.py
+++ #339A - Helpful Maths.py
@@ -1,11 +1,10 @@
-def P8():
-    text = input().strip()
-    split_numbers = text.split('+')
+text = input().strip()
+split_numbers = text.split('+')
 
-    int_nums = [int(num) for num in split_numbers]
+int_nums = [int(num) for num in split_numbers]
 
-    sorted_nums = sorted(int_nums)
+sorted_nums = sorted(int_nums)
 
-    str_nums = [str(num) for num in sorted_nums]
+str_nums = [str(num) for num in sorted_nums]
 
-    print('+'.join(str_nums))
+print('+'.join(str_nums))
