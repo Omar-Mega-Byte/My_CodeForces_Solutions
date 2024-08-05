fbc3b9f9e8dd8d83db80ed88b1f89780afefefc1
Omar Ahmed <omar.tolis2004@gmail.com>
2024-08-05 09:01:57 +0300

    Create #282A - Bit++.py

---
+++ #282A - Bit++.py
@@ -0,0 +1,12 @@
+def P3():
+    x = 0
+    num = int(input())
+
+    for _ in range(num):
+        operation = input().strip()
+        if "++" in operation:
+            x += 1
+        elif "--" in operation:
+            x -= 1
+
+    print(x)
