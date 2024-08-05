0ccfa0030a611821c0859c50a0abf4d34c02043d
Omar Ahmed <omar.tolis2004@gmail.com>
2024-08-05 09:49:31 +0300

    Update #71A - Way Too Long Words.py

--- #71A - Way Too Long Words.py
+++ #71A - Way Too Long Words.py
@@ -1,14 +1,13 @@
-def P1():
-    num = int(input())
-    text = []
+num = int(input())
+text = []
 
-    # Read words into the list
-    for _ in range(num):
-        text.append(input().strip())
+# Read words into the list
+for _ in range(num):
+    text.append(input().strip())
 
-    # Process each word and print the result
-    for word in text:
-        if len(word) > 10:
-            print(word[0] + str(len(word) - 2) + word[-1])
-        elif len(word) <= num or (num < len(word) <= 10):
-            print(word)
+# Process each word and print the result
+for word in text:
+    if len(word) > 10:
+        print(word[0] + str(len(word) - 2) + word[-1])
+    elif len(word) <= num or (num < len(word) <= 10):
+        print(word)
