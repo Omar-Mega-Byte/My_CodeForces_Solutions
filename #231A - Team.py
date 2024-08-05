fe0be5bbfddebdd7694dc5966bbd04183c27beba
Omar Ahmed <omar.tolis2004@gmail.com>
2024-08-05 09:47:24 +0300

    Update #231A - Team.py

--- #231A - Team.py
+++ #231A - Team.py
@@ -1,16 +1,15 @@
-def P2():
-    num = int(input())
-    numbers = []
+num = int(input())
+numbers = []
 
-    # Read numbers into the list
-    for _ in range(num):
-        row = list(map(int, input().split()))
-        numbers.append(row)
+# Read numbers into the list
+for _ in range(num):
+    row = list(map(int, input().split()))
+    numbers.append(row)
 
-    count = 0
+count = 0
 
-    for number in numbers:
-        if sum(number) >= 2:
-            count += 1
+for number in numbers:
+    if sum(number) >= 2:
+        count += 1
 
-    print(count)
+print(count)
