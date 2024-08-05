e339dd5054a42b0ca884fea8bdb68a978649f0f8
Omar Ahmed <omar.tolis2004@gmail.com>
2024-08-05 09:46:25 +0300

    Update #112A - Petya and Strings.py

--- #112A - Petya and Strings.py
+++ #112A - Petya and Strings.py
@@ -1,13 +1,13 @@
-def P7():
-    text1 = input().strip()
-    text2 = input().strip()
 
-    text1 = text1.lower()
-    text2 = text2.lower()
+text1 = input().strip()
+text2 = input().strip()
 
-    if text1 < text2:
-        print(-1)
-    elif text1 > text2:
-        print(1)
-    else:
-        print(0)
+text1 = text1.lower()
+text2 = text2.lower()
+
+if text1 < text2:
+    print(-1)
+elif text1 > text2:
+    print(1)
+else:
+    print(0)
