13198121b9cd619e1406c2af087c4e64ab0550ba
Omar Ahmed <omar.tolis2004@gmail.com>
2024-08-05 09:47:41 +0300

    Update #236A - Boy or Girl.py

--- #236A - Boy or Girl.py
+++ #236A - Boy or Girl.py
@@ -1,13 +1,12 @@
-def P10():
-    text = input().strip()
-    letters = []
-    count = 0
-    for letter in text:
-        if letter not in letters:
-            letters.append(letter)
-            count += 1
+text = input().strip()
+letters = []
+count = 0
+for letter in text:
+    if letter not in letters:
+        letters.append(letter)
+        count += 1
 
-    if count % 2 == 0:
-        print("CHAT WITH HER!")
-    else:
-        print("IGNORE HIM!")
+if count % 2 == 0:
+    print("CHAT WITH HER!")
+else:
+    print("IGNORE HIM!")
