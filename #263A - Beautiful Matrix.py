5f0b7f49a62d7ddd3f60651f092c4c4c0cb7e401
Omar Ahmed <omar.tolis2004@gmail.com>
2024-08-05 09:47:56 +0300

    Update #263A - Beautiful Matrix.py

--- #263A - Beautiful Matrix.py
+++ #263A - Beautiful Matrix.py
@@ -1,15 +1,14 @@
-def P6():
-    numbers = []
-    count = 0
-    target_col = 2
-    target_row = 2
-    # Read numbers into the list
-    for i in range(5):
-        row = list(map(int, input().split()))
-        numbers.append(row)
-        if 1 in row:
-            one_row = i
-            one_col = row.index(1)
+numbers = []
+count = 0
+target_col = 2
+target_row = 2
+# Read numbers into the list
+for i in range(5):
+    row = list(map(int, input().split()))
+    numbers.append(row)
+    if 1 in row:
+        one_row = i
+        one_col = row.index(1)
 
-    count = abs(target_col - one_col) + abs(target_row - one_row)
-    print(count)
+count = abs(target_col - one_col) + abs(target_row - one_row)
+print(count)
